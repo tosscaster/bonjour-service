67ed22f05a0039463021fe013d34e3b3ba569272
Matt Didon <matt@onlx.co.uk>
2024-01-08 12:57:16 +0000

    Update equal-txt.ts
    Handle event where txt a or b can be undefined
    #47

--- src/lib/utils/equal-txt.ts
+++ src/lib/utils/equal-txt.ts
@@ -1,4 +1,5 @@
 export default function equalTxt(a: Record<string, string>, b: Record<string, string>): boolean {
+    if(a === undefined || b === undefined) return false
     let aKeys = Object.keys(a)
     let bKeys = Object.keys(b)
     if(aKeys.length != bKeys.length) return false
